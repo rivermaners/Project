7efdb8ddaa948c17f44b0f1600a81f2f923355d5
rivermaners <river.maners@gmail.com>
2023-04-27 23:03:19 -0500

    Map Update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 40.71823902852354, lng: -73.99581972249696 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
